15585a777620d05625d5aea7b89a63af07bede85
James <jamesdryden7@gmail.com>
2022-01-10 23:00:04 -0500

    created and updated script sheet with code for open active button status on accordion

---
+++ script.js
@@ -0,0 +1,16 @@
+const question = document.querySelectorAll('.accordion-question');
+
+question.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		button.classList.toggle('open');
+		button.nextElementSibling.classList.toggle('open');
+	});
+});
+
+// What I want to say now is if another button is clicked... close the opened class and add that class to the active button
+
+// What I want to say is if the button is clicked, activate open toggle, if clicked again close toggle/window
+
+// function toggle() {
+// 	question.classList.toggle('open');
+// }
